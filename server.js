d76a5e42adf1938f1bbcc5b3785258dcdd95a05c
Cody Yuill <codyyuill@hotmail.com>
2021-03-21 14:39:06 -0400

    setup mongo atlas for deployed db

--- server.js
+++ server.js
@@ -10,7 +10,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessTracker", 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+  );
 
 // routes
 app.use(require("./routes/routes.js"));
