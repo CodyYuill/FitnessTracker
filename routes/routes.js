60674d291f0449eb4771e691825ef256659ce2cc
Cody Yuill <codyyuill@hotmail.com>
2021-03-21 16:08:28 -0400

    setup model correctly to include the day and have the exercises as an array, continue workout works, stats are displayed

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,47 @@
 const router = require("express").Router();
 const Workout = require("../models/workouts.js");
+const mongojs = require("mongojs");
 
 router.get("/exercise", (req, res)=>{
     res.sendFile("/exercise.html", { root: "public" });
 });
 
-//adding an exercise
-router.put("/api/workouts/:id", ({ body }, res)=>{
-    Workout.create(body).then(dbExercise => {
+router.get("/stats", (req, res)=>{
+    res.sendFile("/stats.html", { root: "public" });
+});
+
+router.get("/api/workouts", (req, res)=>{
+    Workout.find().limit(1).sort({day: -1}).then(dbResult => {
+        res.send(dbResult);
+    });
+});
+
+//make a new workout
+router.post("/api/workouts", (req, res)=>{
+    Workout.create({day: new Date()}).then(dbExercise => {
         res.json(dbExercise);
     }).catch(err => {
         res.status(400).json(err);
+    });
+});
+
+//adding an exercise to workout at _id
+router.put("/api/workouts/:id", (req, res)=>{
+    Workout.updateOne({_id: mongojs.ObjectId(req.params.id)}, {$push: {exercises: req.body}})
+        .then(dbExercise => {
+            console.log(dbExercise);
+            res.json(dbExercise);
+        }).catch(err => {
+            res.status(400).json(err);
+        });
+});
+//stuff for stats
+router.get("/api/workouts/range", (req, res)=>{
+    Workout.find({}, (err, dbResult)=>{
+        if(err) 
+            res.send(err);
+        else
+            res.send(dbResult);
     });
 });
 
